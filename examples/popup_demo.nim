601014e1219dd1791844f0efe078606224313538
Jaremy Creechley <creechley@gmail.com>
2026-03-15 16:41:35 -0600

    add xdg_popup

--- examples/popup_demo.nim
+++ examples/popup_demo.nim
@@ -105,7 +105,7 @@ proc popupPlacement(size: IVec2): PopupPlacement =
     anchorRectSize: ivec2(buttonRect.w, buttonRect.h),
     size: PopupSize,
     anchor: Edge.bottomRight,
-    gravity: Edge.topLeft,
+    gravity: Edge.bottomRight,
     offset: ivec2(0, 14),
     constraintAdjustment: {
       PopupConstraintAdjustment.pcaSlideX, PopupConstraintAdjustment.pcaFlipY,
