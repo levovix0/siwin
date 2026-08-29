96ca695e0ed8b2153ebae0404008f5eeddd87fe0
Jaremy Creechley <creechley@gmail.com>
2026-08-29 21:23:30 +0300

    Fix Cocoa modifier recovery and duplicate control input (#48)
    * Fix stuck Cocoa modifier state after focus changes
    
    * Avoid duplicate Cocoa control text input

--- src/siwin/platforms/cocoa/modifierstate.nim
+++ src/siwin/platforms/cocoa/modifierstate.nim
@@ -8,9 +8,12 @@ proc CGEventCreate(source: pointer): CGEventRef {.
   importc, header: "<CoreGraphics/CoreGraphics.h>".}
 proc CGEventGetFlags(event: CGEventRef): CGEventFlags {.
   importc, header: "<CoreGraphics/CoreGraphics.h>".}
+proc CGEventSourceKeyState(stateId: int32, keyCode: uint16): bool {.
+  importc, header: "<CoreGraphics/CoreGraphics.h>".}
 proc CFRelease(cf: pointer) {.importc, header: "<CoreFoundation/CoreFoundation.h>".}
 
 const
+  CgCombinedSessionState = 0'i32
   CgShiftMask = 0x00020000'u64
   CgCtrlMask = 0x00040000'u64
   CgAltMask = 0x00080000'u64
@@ -34,3 +37,6 @@ proc tryCurrentModifierState*(state: var set[ModifierKey]): bool =
   CFRelease(ev)
   state = modifierStateFromCgFlags(flags)
   true
+
+proc currentKeyPressed*(keyCode: uint16): bool =
+  CGEventSourceKeyState(CgCombinedSessionState, keyCode)
